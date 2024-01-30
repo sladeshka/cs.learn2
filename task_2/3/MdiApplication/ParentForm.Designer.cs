9f6bc3a5a3562e1bc44b0054d74448b4d7ef6579
sladeshka <130031mail@mail.ru>
2024-01-30 19:05:26 +0300

    task_2_3

---
+++ task_2/3/MdiApplication/ParentForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿using System.Windows.Forms;
+
+namespace MdiApplication
+{
+    partial class ParentForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ParentForm));
+            FileMenuItem = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            NewMenuItem = new ToolStripMenuItem();
+            ExitMenuItem = new ToolStripMenuItem();
+            WindowMenuItem = new ToolStripMenuItem();
+            WindowCascadeMenuItem = new ToolStripMenuItem();
+            WindowTileMenuItem = new ToolStripMenuItem();
+            toolStrip1 = new ToolStrip();
+            toolStripButton1 = new ToolStripButton();
+            toolStripSeparator1 = new ToolStripSeparator();
+            toolStripButton2 = new ToolStripButton();
+            toolStripButton3 = new ToolStripButton();
+            FileMenuItem.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // FileMenuItem
+            // 
+            FileMenuItem.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, WindowMenuItem });
+            FileMenuItem.Location = new Point(0, 0);
+            FileMenuItem.MdiWindowListItem = WindowMenuItem;
+            FileMenuItem.Name = "FileMenuItem";
+            FileMenuItem.Size = new Size(420, 24);
+            FileMenuItem.TabIndex = 1;
+            FileMenuItem.Text = "MdiMenu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { NewMenuItem, ExitMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "&File";
+            // 
+            // NewMenuItem
+            // 
+            NewMenuItem.Name = "NewMenuItem";
+            NewMenuItem.Size = new Size(98, 22);
+            NewMenuItem.Text = "&New";
+            NewMenuItem.Click += NewMenuItem_Click;
+            // 
+            // ExitMenuItem
+            // 
+            ExitMenuItem.Name = "ExitMenuItem";
+            ExitMenuItem.Size = new Size(98, 22);
+            ExitMenuItem.Text = "&Exit";
+            ExitMenuItem.Click += ExitMenuItem_Click;
+            // 
+            // WindowMenuItem
+            // 
+            WindowMenuItem.DropDownItems.AddRange(new ToolStripItem[] { WindowCascadeMenuItem, WindowTileMenuItem });
+            WindowMenuItem.Name = "WindowMenuItem";
+            WindowMenuItem.Size = new Size(63, 20);
+            WindowMenuItem.Text = "&Window";
+            // 
+            // WindowCascadeMenuItem
+            // 
+            WindowCascadeMenuItem.Name = "WindowCascadeMenuItem";
+            WindowCascadeMenuItem.Size = new Size(118, 22);
+            WindowCascadeMenuItem.Text = "&Cascade";
+            WindowCascadeMenuItem.Click += WindowCascadeMenuItem_Click;
+            // 
+            // WindowTileMenuItem
+            // 
+            WindowTileMenuItem.Name = "WindowTileMenuItem";
+            WindowTileMenuItem.Size = new Size(118, 22);
+            WindowTileMenuItem.Text = "&Tile";
+            WindowTileMenuItem.Click += WindowTileMenuItem_Click;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1, toolStripSeparator1, toolStripButton2, toolStripButton3 });
+            toolStrip1.Location = new Point(0, 24);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(420, 25);
+            toolStrip1.TabIndex = 3;
+            toolStrip1.Text = "toolStrip1";
+            toolStrip1.ItemClicked += toolStrip1_ItemClicked;
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(23, 22);
+            toolStripButton1.Tag = "NewDoc";
+            toolStripButton1.Text = "toolStripButton1";
+            toolStripButton1.ToolTipText = "Create new document";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(6, 25);
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton2.Image = (Image)resources.GetObject("toolStripButton2.Image");
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(23, 22);
+            toolStripButton2.Tag = "Cascade";
+            toolStripButton2.Text = "toolStripButton2";
+            toolStripButton2.ToolTipText = "Windows cascade";
+            toolStripButton2.Click += toolStripButton2_Click;
+            // 
+            // toolStripButton3
+            // 
+            toolStripButton3.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton3.Image = (Image)resources.GetObject("toolStripButton3.Image");
+            toolStripButton3.ImageTransparentColor = Color.Magenta;
+            toolStripButton3.Name = "toolStripButton3";
+            toolStripButton3.Size = new Size(23, 22);
+            toolStripButton3.Tag = "Title";
+            toolStripButton3.Text = "toolStripButton3";
+            toolStripButton3.ToolTipText = "Windows title";
+            // 
+            // ParentForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(420, 320);
+            Controls.Add(toolStrip1);
+            Controls.Add(FileMenuItem);
+            IsMdiContainer = true;
+            MainMenuStrip = FileMenuItem;
+            Name = "ParentForm";
+            Text = "ParentForm";
+            FileMenuItem.ResumeLayout(false);
+            FileMenuItem.PerformLayout();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip FileMenuItem;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem NewMenuItem;
+        private ToolStripMenuItem ExitMenuItem;
+        private ToolStripMenuItem WindowMenuItem;
+        private ToolStripMenuItem WindowCascadeMenuItem;
+        private ToolStripMenuItem WindowTileMenuItem;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripButton toolStripButton2;
+        private ToolStripButton toolStripButton3;
+    }
+}
