b33ee2dbd22f1892cf3b31d4ba2d665530f0ba57
sladeshka <130031mail@mail.ru>
2024-01-30 18:58:02 +0300

    task_1_5

---
+++ task_1/5/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp5
+{
+    public partial class Circle : Form
+    {
+        public Circle()
+        {
+            InitializeComponent();
+        }
+
+        private void Circle_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath shape = new();
+            shape.AddEllipse(0, 0, 400, 400);
+            this.Region = new Region(shape);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
